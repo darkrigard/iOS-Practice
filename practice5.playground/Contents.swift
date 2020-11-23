a669fe8d8a389ba444d46cf47865cc6e0ba6e162
darkrigard <darkrigard@gmail.com>
2020-11-23 21:02:20 +0900

    Day3

---
+++ practice5.playground/Contents.swift
@@ -0,0 +1,65 @@
+import UIKit
+
+//let name1 = "jason"
+//let name2 = "Jason"
+
+//let isJason = name2 == "Jason"
+//var title = ""
+//
+//if isJason {
+//    title = "Jason CEO"
+//} else {
+//    title = "Other CEO"
+//}
+//title
+//
+//title = isJason ? "Jason CEO" : "Other CEO"
+//title
+
+
+//var workHours = 9
+//
+//var salaryPerHours = 24000
+//
+//var workday = 20
+//
+//var salary = workHours * salaryPerHours * workday
+//
+//var extraHours = 0
+//
+//var extraSalary = salaryPerHours * 2
+//
+//var salaryPerExtraHours = 0
+//
+//var totalSalary = 0
+//
+//if extraHours > 0 {
+//    salaryPerExtraHours = extraHours * extraSalary * workday
+//
+//     totalSalary += salaryPerExtraHours
+//
+//    totalSalary += salary
+//    print("급여 = \(Double(totalSalary) * 0.9)")
+//}else {
+//    totalSalary += salary
+//    print("급여 = \(Double(totalSalary) * 0.9)")
+//}
+
+
+//while <#condition#> {
+//    <#code#>
+//}
+
+//var i = 0
+//
+//while i < 10 {
+//    print(i)
+//
+//    if i == 5 {
+//        break
+//    }
+//
+//    i+=1
+//}
+
+
